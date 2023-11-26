c8ec583afc1673b42c2c929af0e8f7598181f87f
Thanos Moschou <130168149+thanosmoschou@users.noreply.github.com>
2023-11-26 19:02:51 +0200

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,35 @@
+## Typing Bot
+This is a simple speed typing bot built with Python. <br>
+It uses easyocr and pyautogui modules of Python. <br>
+
+### How it works
+This works for the following site:
+```https://10fastfingers.com/typing-test/english```
+Keep in mind that if you change some coordinates, it can work for different sites also.
+
+First it takes a screenshot of a specified area that contains some text. 
+Then it uses OCR methods to recognize the text.
+Finally, it writes the text to the specified area within the website.
+It repeats the process for 60 seconds.
+
+### How to run it
+Make sure you have python installed on your pc. <br>
+Open the project's folder in a terminal. <br>
+Type:
+```
+pip install -r requirements.txt
+```
+and then: <br>
+```
+python typingBot.py
+```
+
+### Disclaimers
+You have to prepare the website yourself before you run the script. <br>
+Go to the page and click "Start typing test". Make sure the active window is the browser's window. <br>
+Also due to the fact that it uses OCR methods, it is pretty inconsistent. <br>
+The coordinates are tested for 15 inch screens. <br><br>
+This script is made for educational purposes only. <br>
+It is made to show how you can scrape text from images. <br>
+Use it at your own risk. <br>
+Author is not responsible for any damage you may cause by using this script. <br>
---
+++ typingBot.py
@@ -0,0 +1,47 @@
+"""
+Author: Thanos Moschou
+Description: 
+This is a speed typing bot. It uses pyautogui and easyocr modules.
+First it takes a screenshot of a specified area that contains text. Then it uses easyocr module for optical character recognition. 
+Finally it writes the recognized text to the website's
+specified area.
+Site: https://10fastfingers.com/typing-test/english
+It repeats the process for 60 seconds.
+Disclaimers: You have to prepare the website yourself before you run the script. 
+Go to the page and click "Start typing test". Make sure the active window is the browser's window.
+Also due to the fact that it uses ocr methods, it is pretty inconsistent.
+The coordinates are for 15 inch screens.
+
+It is made for educational purposes only.
+Use it at your own risk.
+
+Last modification date: 26/11/2023
+"""
+
+import pyautogui
+import time
+import easyocr
+from PIL import Image
+
+positionScreenshotX = 325
+positionScreenshotY = 344
+positionMouseX = 545
+positionMouseY = 531
+imageWidth = 1180
+imageHeight = 136
+
+reader = easyocr.Reader(["en"])
+pyautogui.click(positionMouseX, positionMouseY) #click on the text field that will be filled 
+imageName = "words.png"
+
+currTime = 0
+
+while currTime < 30: #this will run for 60 seconds
+    pyautogui.screenshot(imageName, region=(positionScreenshotX, positionScreenshotY, imageWidth, imageHeight))
+    results = reader.readtext(imageName)
+
+    for tup in results:
+        pyautogui.write(tup[1]) #1st element of each tupple has the string I want
+        pyautogui.press("space")
+    
+    currTime += 1
